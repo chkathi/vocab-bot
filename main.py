57b332e2952aecfead4e104cb12530acf1f8838f
Chandu Kathi <chkathi311@gmail.com>
2026-08-18 00:31:23 -0700

    fix: implemented retry logic to find new word if api failed

--- main.py
+++ main.py
@@ -11,21 +11,43 @@ def fetch_word():
 
     return response.json()[0]
 
-
-def fetch_definition(word): 
+def get_url(word, COLLIGIATE_VOCAB_KEY):
     api_key = os.environ.get("COLLIGIATE_VOCAB_KEY")
-    url = f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={api_key}"
+    return f"https://www.dictionaryapi.com/api/v3/references/collegiate/json/{word}?key={api_key}"
 
-    response = requests.get(url)
+
+
+def word_of_the_day(): 
+    status = -1
+
+    # Try the word and if we fail get another word and try again until we get a valid response
+    count = 0
+    while status != 200 or count > 10:
+        word = fetch_word()
+        url = get_url(word, os.environ.get("COLLIGIATE_VOCAB_KEY"))
+        response = requests.get(url)
+
+        status = response.status_code
+        count += 1
+
     data = response.json()[0]
 
-    return data['shortdef'][0]
+    if count < 10: 
+        print(f"\nWord of the day: {word}")
+        print(f"Definition: {data['shortdef'][0]}\n")
+
+        '''
+        if count - 1 > 0:
+            print(f"\nTried {count - 1} times to get a valid word.\n")
+        '''
+
+        return
+
+    print(f"\n Word of the day Timed Out.... Try again later. \n")
+
 
 def main(): 
-    word = fetch_word()
-    definition = fetch_definition(word)
-    print(f"\nWord of the day: {word}")
-    print(f"Definition: {definition}\n")
+    word_of_the_day()
 
 if __name__ == "__main__":
     main()
